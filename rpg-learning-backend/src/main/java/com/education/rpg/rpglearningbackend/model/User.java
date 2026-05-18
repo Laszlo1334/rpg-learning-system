3e06899fd749a761040759a2ee3a5c4f084f3998
Laszlo1334 <mzolotarevich@gmail.com>
2026-05-18 16:14:01 +0300

    minor fix

--- rpg-learning-backend/src/main/java/com/education/rpg/rpglearningbackend/model/User.java
+++ rpg-learning-backend/src/main/java/com/education/rpg/rpglearningbackend/model/User.java
@@ -44,10 +44,6 @@ public class User {
         this.level = (currentXp / 1000) + 1;
     }
 
-    public void setCurrentXp(Long currentXp) {
-        this.currentXp = currentXp.intValue();
-        this.level = (int) (currentXp / 1000) + 1;
-    }
 
     @Column(nullable = false)
     private Integer gold = 0; // Поточний баланс монет (для Ачіверів)
